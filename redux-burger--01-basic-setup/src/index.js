0eb70467824dc6791bff4db99b2434c5bf8646a9
Tushar More <Tushar.More@sapiens.com>
2023-01-11 11:08:33 +0530

    Redux advanced

--- redux-burger--01-basic-setup/src/index.js
+++ redux-burger--01-basic-setup/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore ,applyMiddleware, compose} from 'redux';
-import {thunk} from 'redux-thunk'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -14,7 +13,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // const store = createStore(reducer, /* preloadedState, */ compose(
 //     applyMiddleware(...middleware)
 //   ));
-const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer,composeEnhancers(applyMiddleware()));
 
 const app = (
     <Provider store={store}>
